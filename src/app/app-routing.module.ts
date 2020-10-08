aa37f21b464e8e8daae4067dd13e4f96891f6a2c
Tania Perrella <64112114+taniaperrella@users.noreply.github.com>
2020-10-08 02:50:19 -0300

    Upload do projeto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { CharactersComponent } from './characters/characters.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SearchCharacterFormComponent } from './search-character-form/search-character-form.component';
+
+
+
+const routes: Routes = [
+{
+  path: '', redirectTo: '/characters', pathMatch: 'full'
+},
+{
+  path: 'characters', component: CharactersComponent
+},
+{
+  path: 'search-character-form', component: SearchCharacterFormComponent
+}
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
